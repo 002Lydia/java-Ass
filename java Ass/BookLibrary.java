80d9e2237f393ffefadfd506b0cec28c9a2b093c
002Lydia <150788216+002Lydia@users.noreply.github.com>
2023-11-17 18:24:03 +0300

    Add files via upload

---
+++ java Ass/BookLibrary.java
@@ -0,0 +1,11 @@
+public class BookLibrary {
+public static void main(String[]args){
+System.out.println("Title:STAR SHINE");
+System.out.println("Publisher: Syk Element Publisher");
+System.out.println("Author: Lydia TheOne");
+System.out.println("ISBN:547698");
+System.out.println("Year:2034");
+}
+}
+
+
---
+++ java Ass/StudentDetails.java
@@ -0,0 +1,12 @@
+public class  StudentDetails{
+public  static void main (String[]args){
+System.out.println("Course:DIT");
+System.out.println("Name:Lydia Otieno");
+
+System.out.println("Your Registration Number:22/06485");
+System.out.println("Date Of Birth:2/1/2002");
+
+System.out.println("Vision:Touching The World");
+System.out.println("Your Favorite Programming Language:Java");
+}
+}
